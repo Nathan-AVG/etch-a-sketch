c0aa7e8f1e8ab76f0adfa8a378a25efe2ef89f52
Nathan <rensly2208@gmail.com>
2022-09-04 13:47:34 +0800

    note: addeventlistener when creating elements

---
+++ apps.js
@@ -0,0 +1,94 @@
+//buttons
+const pallette = document.querySelector('.tools input');
+const color = document.querySelector('.tool.color');
+const rainbow = document.querySelector('.tool.rainbow');
+const eraser = document.querySelector('.tool.eraser');
+const clear = document.querySelector('.tool.clear');
+//board contents
+const board = document.querySelector('.boardheight');
+const boardheight = document.querySelector('.board');
+//sliders
+const sliderValue = document.querySelector('span');
+const inputSlider = document.querySelector('.size');
+//tools
+const tools = document.querySelectorAll('.tool');
+
+//initialization
+let value = 32;
+draw();
+
+let colorUser = 'white';
+
+color.addEventListener('click', function(){
+    lighten();
+    this.classList.add('selected');
+    let colorUser = pallette.value;
+    changecolor(colorUser);
+    pallette.oninput = (()=>{
+        let colorUser = pallette.value;
+        changecolor(colorUser);
+    });
+});
+
+eraser.addEventListener('click', function(){
+    lighten();
+    this.classList.add('selected');
+    changecolor('white');
+});
+
+
+//range slider display
+inputSlider.oninput = (()=>{
+    let value = inputSlider.value;
+    sliderValue.textContent = value;
+    sliderValue.style.left = ((value/64 * 100)- 9) + "%";
+    sliderValue.classList.add('show');
+});
+
+inputSlider.onblur = (()=>{
+    sliderValue.classList.remove('show');
+})
+
+//change grid size
+inputSlider.onchange = (()=>{
+    clearScreen();
+    draw();
+});
+
+//function
+function lighten(){ //lightens every other tool button
+    tools.forEach(tool => tool.classList.remove('selected'));
+}
+
+function clearScreen(){
+    let value = inputSlider.value;
+
+    while(board.firstChild){
+        const lastElement = board.lastChild;
+        board.removeChild(lastElement);
+    } 
+}
+
+function draw(){
+    const height = boardheight.offsetHeight;
+    let value = inputSlider.value;
+    const magicnum = (height/value);
+
+    for(let i = 0; i < value**2; i++){
+        const newBoard = document.createElement('div');
+        newBoard.addEventListener('mouseover', drawcolor);
+        newBoard.className = 'block';
+
+        newBoard.style.cssText = `height: ${magicnum}px; width: ${magicnum}px; border: 1px solid black`;
+        board.appendChild(newBoard);
+    }
+}
+
+function drawcolor(){
+    this.style.background = colorUser
+}
+
+function changecolor(choice){
+    colorUser = choice;
+}
+
